04a0d956af99315884d880dbe88f475b16d31fdf
wukong <213dormitory@gmail.com>
2018-08-06 19:26:37 +0800

    快速排序答疑

--- src/main/java/com/practice/wukong/algorithm/QuickSort.java
+++ src/main/java/com/practice/wukong/algorithm/QuickSort.java
@@ -6,6 +6,13 @@ import java.util.Arrays;
  * Created by WuKong on 2018/8/5.
  */
 public class QuickSort {
+    /**
+     * 快速排序为什么要以基准值得另一侧开始比较？
+     * 逻辑思维，关注点在相遇的时候，从左侧先走相遇的时候可以保证左边的都是小于标准值的，但是相遇时候的值是要大于标准值（或者已经走到最右边都没有大于标准值的），从右侧走正好是右边都是大于标准值的，但是相遇的时候值是小于标准值（或者走到 最左边都没有小于标准值）。所以要从基数的对面开始。两种情况，就看你从哪开始扫描。不一定非要从右边开始哦！也可以从左边的，那标准值就选右边的。
+     * http://developer.51cto.com/art/201403/430986.htm
+     * https://blog.csdn.net/w282529350/article/details/50982650
+     *
+     */
 
     public static void quicksort(int[] arrays,int start,int end){
         if(start<end){
